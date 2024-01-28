a3da090e8533bf953bcb64149666934a2768f9da
RezaRashidi <reza9300@gmail.com>
2024-01-28 14:42:44 +0330

    f ini

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/p
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-e&8aow7-3zkqyuhtkqas5q+$5*qp__&^8-p8)hpihru)ag2^#8"
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tadbirbodjeh",
 ]
 
 MIDDLEWARE = [
